1b857cb2414227cf5e12b669b7062949a35845bb
davegonzalez <gonzalez.dalex@gmail.com>
2013-10-05 18:34:26 -0700

    Moved config info into app.js and out window view, created sample directive

--- public/js/app.js
+++ public/js/app.js
@@ -1,3 +1,19 @@
-window.app = angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system']);
+angular.module('mean', ['ngCookies', 'ngResource', 'ui.bootstrap', 'ui.route', 'mean.system', 'mean.directives'])
+  .config(['$routeProvider',
+      function($routeProvider) {
+          $routeProvider.
+          when('/', {
+              templateUrl: 'views/index.html'
+          }).
+          otherwise({
+              redirectTo: '/'
+          });
+      }
+  ]).config(['$locationProvider', //Setting HTML5 Location Mode
+      function($locationProvider) {
+          $locationProvider.hashPrefix("!");
+      }
+  ]);
 
 angular.module('mean.system', []);
+angular.module('mean.directives', []);
---
+++ public/js/directives.js
@@ -0,0 +1,4 @@
+angular.module('mean.directives', [])
+  .directive('sample-directive', function() {
+    // insert directive info here!
+  });
